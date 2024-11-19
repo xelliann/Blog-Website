fc304273aadd524188353bd107f6fc88cc3cc532
unknown <xelliann>
2024-11-19 23:18:05 +0530

    first commit

---
+++ admin/config/constants.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+define('ROOT_URL','http://localhost/blog/');
+define('DB_HOST','localhost');
+define('DB_USER','mohit');
+define('DB_PASS','admin1234');
+define('DB_NAME','blog');
+
+if (!isset($_SESSION['user-id'])) {
+    header("location: " . ROOT_URL . "logout.php");
+    //destroy all sessions and redirect user to login page
+    session_destroy();
+    die();
+    header("location: " . ROOT_URL . "signin.php");
+}
